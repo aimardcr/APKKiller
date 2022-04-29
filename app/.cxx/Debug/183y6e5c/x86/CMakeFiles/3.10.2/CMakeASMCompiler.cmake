0c7c601982d4fbebeec0d0a7ce1f6cbc60da93dc
aimardcr <aimaradhitya3@gmail.com>
2022-04-29 22:35:34 +0800

    Major changes.

---
+++ app/.cxx/Debug/183y6e5c/x86/CMakeFiles/3.10.2/CMakeASMCompiler.cmake
@@ -0,0 +1,17 @@
+set(CMAKE_ASM_COMPILER "C:/Users/ASUS/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/bin/clang.exe")
+set(CMAKE_ASM_COMPILER_ARG1 "")
+set(CMAKE_AR "C:/Users/ASUS/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/bin/i686-linux-android-ar.exe")
+set(CMAKE_ASM_COMPILER_AR "CMAKE_ASM_COMPILER_AR-NOTFOUND")
+set(CMAKE_RANLIB "C:/Users/ASUS/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/bin/i686-linux-android-ranlib.exe")
+set(CMAKE_ASM_COMPILER_RANLIB "CMAKE_ASM_COMPILER_RANLIB-NOTFOUND")
+set(CMAKE_LINKER "C:/Users/ASUS/AppData/Local/Android/Sdk/ndk/21.4.7075529/toolchains/llvm/prebuilt/windows-x86_64/bin/i686-linux-android-ld.exe")
+set(CMAKE_ASM_COMPILER_LOADED 1)
+set(CMAKE_ASM_COMPILER_ID "Clang")
+set(CMAKE_ASM_COMPILER_VERSION "")
+set(CMAKE_ASM_COMPILER_ENV_VAR "ASM")
+
+
+set(CMAKE_ASM_IGNORE_EXTENSIONS h;H;o;O;obj;OBJ;def;DEF;rc;RC)
+set(CMAKE_ASM_LINKER_PREFERENCE 0)
+
+
